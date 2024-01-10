34ce3c6c8223cd32024fee16e95bcb09e2e74c1d
Elena <aleklenoch@gmail.com>
2024-01-10 19:48:49 +0200

    Race time

--- src/CarRace/Race.java
+++ src/CarRace/Race.java
@@ -1,31 +1,46 @@
 package CarRace;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static java.lang.Thread.sleep;
 
 public class Race {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-         int distance=1000;
+    public static AtomicLong startRaceTime;
+    public static void main(String[] args) throws InterruptedException {
 
+        int distance=1000;
+
+        int numberOfCar=3;
+
+        CountDownLatch latch = new CountDownLatch(numberOfCar);
          List<RaceCarRunnable> cars=new ArrayList<>();
 
-         cars.add(new RaceCarRunnable("audi",160,distance));
-         cars.add(new RaceCarRunnable("vw",160,distance));
-         cars.add(new RaceCarRunnable("toyota",160,distance));
+         cars.add(new RaceCarRunnable("Audi",160,distance,latch));
+         cars.add(new RaceCarRunnable("VW",160,distance,latch));
+         cars.add(new RaceCarRunnable("Toyota",160,distance,latch));
 
          List< Thread > carsRace=new ArrayList<>();
          for (var car:cars ) {
              carsRace.add(new Thread(car));
          }
 
+
         startRace(carsRace);
-        System.out.println("End");
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void  startRace(List<Thread> cars){
+
+        startRaceTime = new AtomicLong(System.currentTimeMillis());
         Thread tr=  new Thread(new Runnable() {
             @Override
             public void run() {
@@ -49,5 +64,11 @@ public class Race {
         });
         tr.start();
     }
+    public static String convertToTime(long time) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("ss.SSS");
+        dateFormat.setTimeZone(java.util.TimeZone.getTimeZone("UTC"));
+        Date date = new Date(time);
 
+        return dateFormat.format(date)+" секунд";
+    }
 }
--- src/CarRace/RaceCarRunnable.java
+++ src/CarRace/RaceCarRunnable.java
@@ -1,17 +1,23 @@
 package CarRace;
 
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
+import static CarRace.Race.startRaceTime;
+import static java.lang.Thread.activeCount;
 import static java.lang.Thread.sleep;
 
 public class RaceCarRunnable extends Car{
 private int passed;
 private int distance;
+private long finishTime;
+protected CountDownLatch latch;
 private boolean isFinish;
-    public RaceCarRunnable(String name, int maxSpeed, int distance) {
+    public RaceCarRunnable(String name, int maxSpeed, int distance,CountDownLatch latch) {
         super(name, maxSpeed);
         this.distance=distance;
         passed = 0;
+        this.latch=latch;
     }
     public int  getRandomSpeed(){
         Random rand = new Random();
@@ -32,10 +38,18 @@ public void run(){
         if(passed >= distance){
             passed=distance;
             isFinish = true;
+            latch.countDown();
+           finishTime=  System.currentTimeMillis() - Race.startRaceTime.get() ;
+            System.out.println(name + " FINISHED! time in race = "+ Race.convertToTime(finishTime));
+            return;
         }
         print();
     }
 }
+
+    public long getFinishTime() {
+        return finishTime;
+    }
 
     public int getPassed() {
         return passed;
